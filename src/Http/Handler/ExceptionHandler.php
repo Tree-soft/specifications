6112978916c9f8d0624b56ca114a04043ce3aa04
Sergei Melnikov <me@rnr.name>
2017-01-26 14:37:51 +0700

    Added ExceptionHandler.

---
+++ src/Http/Handler/ExceptionHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mildberry\Specifications\Http\Handler;
+
+use Illuminate\Foundation\Exceptions\Handler;
+use Exception;
+use Mildberry\Specifications\Exceptions\EntityValidationException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+
+/**
+ * @author Sergei Melnikov <me@rnr.name>
+ */
+class ExceptionHandler extends Handler
+{
+    /**
+     * @param Exception $e
+     *
+     * @return Exception
+     */
+    protected function prepareException(Exception $e)
+    {
+        $e = parent::prepareException($e);
+
+        if ($e instanceof EntityValidationException) {
+            $e = new UnprocessableEntityHttpException($e->getMessage(), $e, $e->getCode());
+        }
+
+        return $e;
+    }
+}
