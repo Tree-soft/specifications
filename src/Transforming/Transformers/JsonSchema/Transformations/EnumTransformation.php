7e2bb54a070be5adc7e66191ac52650cbc7545c4
Sergei Melnikov <s.melnikov@carprice.ru>
2017-03-23 18:15:18 +0300

    Added abstract enum transformation

---
+++ src/Transforming/Transformers/JsonSchema/Transformations/EnumTransformation.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Mildberry\Specifications\Transforming\Transformers\JsonSchema\Transformations;
+
+use Mildberry\Specifications\Transforming\TransformerFactory;
+use Mildberry\Specifications\Transforming\Transformers\ValueDescriptor;
+
+/**
+ * Class EnumTransformation.
+ */
+abstract class EnumTransformation extends AbstractTransformation
+{
+    /**
+     * @return mixed
+     */
+    abstract public function getSchema();
+
+    /**
+     * @param $value
+     *
+     * @return mixed
+     */
+    abstract public function getEnum($value);
+
+    /**
+     * @param ValueDescriptor $from
+     * @param ValueDescriptor $value
+     * @param callable $next
+     *
+     * @return mixed
+     */
+    public function apply(ValueDescriptor $from, ValueDescriptor $value, $next): ValueDescriptor
+    {
+        /**
+         * @var TransformerFactory $factory
+         */
+        $factory = $this->container->make(TransformerFactory::class);
+
+        $schema = $this->getSchema();
+
+        $transformer = $factory->create($from->getSchema(), $schema);
+
+        $enum = new ValueDescriptor();
+
+        $enum
+            ->setValue(
+                $this->getEnum($transformer->transform($from->getValue()))
+            )
+            ->setSchema($schema);
+
+        return $next($enum, $value);
+    }
+}
